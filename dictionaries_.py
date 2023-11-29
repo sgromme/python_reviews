5ebf705ab7b6c09bc021eb358a29829791f58074
sgromme <wills1377@gmail.com>
2023-11-29 08:39:46 -0500

    Spelling

--- dictionaries_.py
+++ dictionaries_.py
@@ -1,2 +1,2 @@
-# test commit , it seems that Anaconda launched VSC doesn't recogonize git users correctly,
+# test commit , it seems that Anaconda launched VSC doesn't recognize git users correctly,
 # is this because Anaconda is not launched with Admin?
