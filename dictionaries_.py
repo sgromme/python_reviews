6ed9d895c7fffebdc22b95c381e96fff23c6c0ae
sgromme <wills1377@gmail.com>
2024-06-18 14:50:55 -0400

    git test.

--- dictionaries_.py
+++ dictionaries_.py
@@ -1,2 +1,9 @@
 # test commit , it seems that Anaconda launched VSC doesn't recognize git users correctly,
 # is this because Anaconda is not launched with Admin?
+
+import pandas
+
+
+print ("test")
+
+
